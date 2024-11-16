1c14086c9da1afd49ad88df44f240bcc34737aeb
Ale Guzman <a01736793@tec.mx>
2024-11-16 13:53:35 -0600

    vistas colaborador

---
+++ iOSGreenMatesColab/CrearTallerViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CrearTallerViewController.swift
+//  iOSGreenMatesColab
+//
+//  Created by Alejandro Guzman on 08/11/24.
+//
+
+import UIKit
+
+class CrearTallerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
